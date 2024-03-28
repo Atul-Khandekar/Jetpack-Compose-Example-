7078cf8a5a76eeb238993b0b1aff233f02c59f88
Atul Khandekar <khandekaratul95@gmail.com>
2024-03-28 20:45:41 +0530

    Add first composable

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -3,44 +3,59 @@ package com.example.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposeexample.ui.theme.JetpackComposeExampleTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            PreviewMessageCard()
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(message: Message) {
+
+    Row (modifier = Modifier.padding(all = 10.dp)){
+
+        Image(painter = painterResource(id = R.drawable.ic_profile), contentDescription = "Sharingan from infinite tsukuyomi"
+
+            ,modifier = Modifier
+                .size(48.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+        Column{
+            Text(text = message.author)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = message.body)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackComposeExampleTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(Message("Atul", "You have to stop wasting time for once buddy"))
 }
+
+data class Message(val author: String, val body: String)
